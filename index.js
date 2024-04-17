2541e96775887ce4ff4ebc73912719b993f313f2
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-17 09:26:57 +0300

    completed domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('the DOM has been loaded')
+    let paragraph = document.querySelector('p');
+    paragraph.textContent = ('This is really cool!');
+})
